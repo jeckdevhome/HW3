e22dd2c0a2e88fd24b1a89e8a9927f1579c408ce
Eugen Gritsiuk <eugen.grytsiuk@gmail.com>
2018-05-30 17:54:20 +0300

    add ICalc

---
+++ HomeWork/Calc.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork
+{
+    class Calc : ICalc
+    {
+        public Calc()
+        {
+            
+        }
+        public double result, value11, value22;
+
+        public double Extract(double value11, double value22)
+        {
+        result = value11 - value22;
+            return result;
+        }
+
+        public double Add(double value11, double value22)
+        {
+            result = value11 + value22;
+            return result;
+        }
+        
+    }
+}
---
+++ HomeWork/ICalc.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork
+{
+    public delegate void RetRestoCalc(double value1, double value2);
+    interface ICalc
+    {
+        double Add(double value1, double value2);
+        double Extract(double value1, double value2);
+
+    }
+}
