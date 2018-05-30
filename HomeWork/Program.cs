8d0b658c105589b836b9fdeac19e4d799fdf2680
Eugen Gritsiuk <eugen.grytsiuk@gmail.com>
2018-05-30 13:51:56 +0300

    cw

--- HomeWork/Program.cs
+++ HomeWork/Program.cs
@@ -9,9 +9,11 @@ namespace HomeWork
     class Program
     {
         //
-
+        
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello");
+            Console.ReadLine();
         }
     }
 }
